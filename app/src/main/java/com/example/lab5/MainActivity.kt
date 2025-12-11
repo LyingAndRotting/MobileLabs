cdcae5e283c1e0cb5363ba3babaffba3d2573a65
Роман Лали <suicideghost666@vk.com>
2025-12-11 21:34:08 +0500

    добавление калькулятора чаевых

--- app/src/main/java/com/example/lab5/MainActivity.kt
+++ app/src/main/java/com/example/lab5/MainActivity.kt
@@ -1,13 +1,20 @@
 package com.example.lab5
 
+import android.icu.text.NumberFormat
 import android.os.Bundle
 import android.transition.Slide
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
 import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
@@ -16,6 +23,8 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.intl.Locale
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -26,14 +35,14 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             Lab5Theme {
-                ArtGalleryScreen()
+                //ArtGalleryScreen()
+                TipCalculator()
             }
         }
     }
 }
 
 @Composable
-@Preview
 fun ArtGalleryScreen() {
     data class Slide(val imageRes: Int, val title: String, val artist: String, val year: String)
     var currentSlide by remember { mutableStateOf(0) }
@@ -98,3 +107,74 @@ fun ArtGalleryScreen() {
         }
     }
 }
+
+@Composable
+@Preview(showBackground = true)
+fun TipCalculator() {
+    var billAmount by remember { mutableStateOf("") }
+    var tipPercentage by remember { mutableStateOf("") }
+    var roundUp by remember { mutableStateOf(false) }
+
+    val amount = billAmount.toDoubleOrNull() ?: 0.0
+    val percentage = tipPercentage.toDoubleOrNull() ?: 0.0
+
+    var tip by remember(amount, percentage, roundUp) {
+        var calculatedTip = percentage / 100 * amount
+        if (roundUp) {
+            calculatedTip = kotlin.math.ceil(calculatedTip)
+        }
+        mutableStateOf(calculatedTip)
+    }
+
+
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(40.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(
+            text = "Посчитайте чаевые",
+            modifier = Modifier.align(Alignment.Start)
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+
+        OutlinedTextField(
+            value = billAmount,
+            onValueChange = { billAmount = it },
+            label = { Text("Сумма счета") },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+            modifier = Modifier.fillMaxWidth()
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+
+        OutlinedTextField(
+            value = tipPercentage,
+            onValueChange = { tipPercentage = it },
+            label = { Text("Процент чаевых") },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+            modifier = Modifier.fillMaxWidth()
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Text("Округлить?")
+            Switch(
+                checked = roundUp,
+                onCheckedChange = { roundUp = it }
+            )
+        }
+        Spacer(modifier = Modifier.height(24.dp))
+        Text(
+            text = String.format("Сумма чаевых: %.2f₽",tip),
+            fontSize = 30.sp
+        )
+    }
+}
+
